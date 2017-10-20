46009a6074ed92b096a15e98e7a36f6356950e0b
Lucianolo <luciano.ursache@gmail.com>
2017-10-20 16:55:11 +0200

    Changed db from local to mlabs

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	database: 'mongodb://localhost/passport',
+	database: 'mongodb://heroku_1plzf40p:3g3olggt5ohu7otunch18fhhtj@ds125365.mlab.com:25365/heroku_1plzf40p',
 	secret: 'addressBookApi'
 }
